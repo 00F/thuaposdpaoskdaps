db635802064076f6cfdd51c921dc5605dd4bf237
` 0ff <39999036+00F@users.noreply.github.com>
2018-11-22 18:28:33 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const developers = ['488698374560677888','320423357709549568'];
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("--",{type: 'LISTENING'})
+  client.user.setStatus("dnd")
+});
+
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!developers.includes(message.author.id)) return;
+
+if (message.content.startsWith('!w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+  client.user.setStatus("dnd")
+    message.channel.send(`تم تغير الواتشنق الى ** ${argresult} \ ** `)
+} else 
+if (message.content.startsWith('!Le')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+  client.user.setStatus("dnd")
+    message.channel.send(`تم تغير اللسننق الى ** ${argresult} \ ** `)
+} else 
+  if (message.content.startsWith('!p')) {
+client.user.setActivity(argresult , {type:'PLAYING'});
+  client.user.setStatus("dnd")
+    message.channel.send(`تم تغير البلاينق الى  ** ${argresult} \ ** `)
+} else 
+if (message.content.startsWith('!st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/MeeRcY");
+  client.user.setStatus("dnd")
+   message.channel.send(`تم تغير الستريمنق الى ** ${argresult} \ ** `)
+}
+});
+
+client.login(process.env.THUNDER);
